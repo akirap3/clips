7ae2c325d7be028017fdd860b932979cbc9b14e9
akirap3 <akirapf3@gmail.com>
2022-02-20 23:58:29 +0800

    Add authentication after registraion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,47 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 import IUser from '../models/user.model';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+  private userCollection: AngularFirestoreCollection<IUser>;
+  public isAuthenticated$: Observable<boolean>;
+
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+    this.userCollection = db.collection('users');
+    this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+  }
 
   public async createUser(userData: IUser) {
+    if (!userData.password) {
+      throw new Error('Password not provided');
+    }
+
     const userCred = await this.auth.createUserWithEmailAndPassword(
       userData.email,
       userData.password
     );
-    await this.db.collection('users').add({
+
+    if (!userCred.user) {
+      throw new Error("User can't be found");
+    }
+
+    await this.userCollection.doc(userCred.user.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
+    });
+
+    await userCred.user.updateProfile({
+      displayName: userData.name,
     });
   }
 }
